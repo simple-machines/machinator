42788a02746826b5bbe963561790c06e71ce91cf
Thomas Sutton <me@thomas-sutton.id.au>
2021-09-01 11:07:41 +1000

    Python JSON de-/serialisation

--- machinator-python/src/Machinator/Python/Scheme/Types/Codegen.hs
+++ machinator-python/src/Machinator/Python/Scheme/Types/Codegen.hs
@@ -22,23 +22,23 @@ import qualified Text.PrettyPrint.Annotated.WL as WL
 
 -- | Generates a type declaration for the given definition.
 genTypesV1 :: Definition -> Text
-genTypesV1 def@(Definition _ mDoc _) =
-  renderText $ genTypesV1' def
+genTypesV1 def = renderText $ genTypesV1' def
 
 -- | Generates a type declaration for the given definition.
 genTypesV1' :: Definition -> Doc a
 genTypesV1' (Definition name@(Name n) mDoc dec) =
   case dec of
-    Variant (c1 :| cts) 
+    Variant (c1 :| cts)
       | isEnum (c1:cts) -> enum name mDoc (c1:cts)
       | otherwise       -> WL.vsep $
-                               dataclass n Nothing mDoc []
+                               dataclass name Nothing mDoc []
                              : fmap (uncurry3 (genConstructorV1 name)) (c1:cts)
 
     Record fts ->
       genRecordV1 name mDoc fts
 
-    Newtype ft@(_, t) ->
+    Newtype (_, t) ->
+      -- TODO: We may want to do something a little more useful here.
       WL.vsep [
         comment mDoc,
         WL.hsep [text n, WL.char '=', genTypeV1 t]
@@ -48,6 +48,8 @@ genTypeV1 :: Type -> Doc a
 genTypeV1 ty =
   case ty of
     Variable (Name n) ->
+      -- NB: We don't actually do anything with typevars. This case will probably
+      -- result in an error in the generated code.
       text n
     GroundT g ->
       case g of
@@ -73,8 +75,8 @@ genTypeV1 ty =
       string "Optional" <> WL.brackets (genTypeV1 t2)
 
 genConstructorV1 :: Name -> Name -> Maybe Docs -> [(Name, Type)] -> Doc a
-genConstructorV1 (Name extends) (Name constructorName) mDoc tys =
-    dataclass constructorName (Just extends) mDoc tys
+genConstructorV1 (Name extends) name =
+  dataclass name (Just extends)
 
 -- | Generates a naked record for the given definition.
 --
@@ -87,7 +89,7 @@ genRecordV1 (Name n) _ [] =
   WL.hang 2 $
     text "case object" <+> text n
 
-genRecordV1 (Name n) mDoc fts = dataclass n Nothing mDoc fts
+genRecordV1 name mDoc fts = dataclass name Nothing mDoc fts
 
 -- -----------------------------------------------------------------------------
 
@@ -107,15 +109,23 @@ comment :: Maybe Docs -> Doc a
 comment Nothing = WL.mempty
 comment (Just (Docs docs)) = WL.vsep $ fmap (\t -> text "#" WL.<+> text (T.strip t)) $ T.lines docs
 
-docstring :: Maybe Docs -> [Doc a]
-docstring Nothing = []
-docstring (Just (Docs docs)) =
+classDocstring :: Maybe Docs -> [(Name, Type)] -> [Doc a]
+classDocstring Nothing _ = []
+classDocstring (Just (Docs docs)) flds =
   let
     open  = string "\"\"\""
     close = string "\"\"\""
     trimmed = T.stripEnd (T.unlines (T.strip <$> T.lines docs))
+    args = WL.vsep [
+        WL.softbreak ,
+        text "Args:",
+        WL.indent 2 . WL.vsep $
+          fmap (\(Name n, ty) -> text n WL.<+> WL.parens (genTypeV1 ty) <> WL.char ':' WL.<+> string "A field") flds
+      ]
   in (:[]) . WL.group $
-    open <> WL.align (WL.pretty trimmed) <> close
+    open <> WL.align (
+      WL.pretty trimmed WL.<#> args
+    ) WL.<#> close
 
 -- | Converts a curried function to a function on a triple.
 uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
@@ -126,47 +136,47 @@ enum :: Name -> Maybe Docs -> [(Name, Maybe Docs, [(Name, Type)])] -> Doc a
 enum (Name n) mDoc ctors =
     WL.vsep [
       string "class" WL.<+> text n WL.<> WL.parens (string "enum.Enum") WL.<> ":",
-      WL.indent 4 . WL.vsep $ docstring mDoc <> fmap (\(Name m, _, []) -> WL.hsep [text m, WL.char '=', WL.dquotes $ text m]) ctors
+      WL.indent 4 . WL.vsep $ classDocstring mDoc [] <> fmap (\(Name m, _, []) -> WL.hsep [text m, WL.char '=', WL.dquotes $ text m]) ctors
     ] WL.<> WL.line
 
 
 -- | Generates a Python dataclass.
-dataclass :: Text -> Maybe Text -> Maybe Docs -> [(Name, Type)] -> Doc a
-dataclass name super mDoc flds = 
+dataclass :: Name -> Maybe Text -> Maybe Docs -> [(Name, Type)] -> Doc a
+dataclass name@(Name n) super mDoc flds =
   WL.vsep [
     string "@dataclass(frozen=True)",
-    string "class" <+> text name <> extends <> string ":",
-    WL.indent 4 . WL.vsep $ (docstring mDoc <> fields flds <> serde name flds),
+    string "class" <+> text n <> extends <> string ":",
+    WL.indent 4 . WL.vsep $ (classDocstring mDoc flds <> fields flds <> serde name flds),
     WL.line
   ]
   where
     extends =
       case super of
         Nothing -> WL.mempty
-        Just n -> WL.parens $ text n
+        Just sn -> WL.parens $ text sn
 
 fields :: [(Name, Type)] -> [Doc a]
 fields = fmap (\(Name n, t) -> text n WL.<> WL.char ':' WL.<+> genTypeV1 t)
 
-serde :: Text -> [(Name, Type)] -> [Doc a]
-serde _ _ =
+serde :: Name -> [(Name, Type)] -> [Doc a]
+serde n flds =
   [ WL.mempty
-  , classmethod "json_schema" []
-  , classmethod "from_json" [(Name "data", Right "dict")]
-  , method "to_json" []
+  , classmethod $ method "json_schema" [] (string "pass")
+  , generateFromJson n flds
+  , generateToJson n flds
   ]
 
-classmethod name args =
+classmethod :: Doc a -> Doc a
+classmethod body =
     WL.vsep [
-      string "@classmethod"
-    , string "def" WL.<+> text name WL.<> WL.encloseSep WL.lparen WL.rparen WL.comma (string "cls" : fmap arg args) WL.<> WL.char ':'
-    , WL.indent 4 $ string "pass"
-    ]  WL.<> WL.line
+      string "@classmethod", body
+    ]
 
-method name args =
+method :: Text -> [(Name, Either Type Text)] -> Doc a -> Doc a
+method name args body =
     WL.vsep [
       string "def" WL.<+> text name <> WL.encloseSep WL.lparen WL.rparen WL.comma (string "self" : fmap arg args) WL.<> WL.char ':'
-    , WL.indent 4 $ string "pass"
+    , WL.indent 4 body
     ] WL.<> WL.line
 
 arg :: (Name, Either Type Text) -> Doc a
@@ -180,3 +190,60 @@ isEnum cs = go cs
     go ((_, _, []):rs) = go rs
     go _ = False
 
+generateToJson :: Name -> [(Name, Type)] -> Doc a
+generateToJson cls flds =
+    method "to_json" [(Name "self", Left (Variable cls))] $ WL.vsep [
+      tripleQuote (text "Generate dictionary ready to be serialised to JSON."),
+      text "return dict" <> WL.parens (
+        WL.line <>
+        WL.indent 4 (WL.vsep (fmap serialiseField flds)) <>
+        WL.line
+      )
+    ]
+  where
+    serialiseField (Name f, ty) =
+      text f <> "=" <> serialiseType ("self." <> text f) ty
+
+generateFromJson :: Name -> [(Name, Type)] -> Doc a
+generateFromJson (Name n) flds =
+    classmethod . method "from_json" [(Name "data", Right "dict")] $ WL.vsep [
+      tripleQuote (text "Validate and parse JSON data into an instance of " <> text n <> "."),
+      text "return" WL.<+> text n <> WL.parens
+        (WL.line <> (WL.indent 4 . WL.vsep $ fmap parseField flds) <> WL.line)
+    ]
+  where
+    parseField (Name f, ty) =
+      text f <> "=" <> parseType ("data.get(\"" <> f <> "\")") ty
+
+tripleQuote :: Doc a -> Doc a
+tripleQuote s = text "\"\"\"" <> s <> text "\"\"\""
+
+serialiseType :: Doc a -> Type -> Doc a
+serialiseType value ty = case ty of
+  Variable na -> value <> ".to_json()"
+  GroundT gr -> case gr of
+    StringT -> value
+    BoolT -> value
+    IntT -> value
+    LongT -> value
+    DoubleT -> value
+    UUIDT -> "str(" <> value <> ")"
+    DateT -> value <> ".strfmt('%Y-%m-%d')"
+    DateTimeT -> value <> ".strfmt('%Y-%m-%dT%H:%M:%S.%f%z')"
+  ListT ty' -> "[" <> serialiseType "v" ty' <> " for v in " <> value <> "]"
+  MaybeT ty' -> "(lambda v: " <> serialiseType "v" ty' <> " if v is not None else None)(" <> value <> ")"
+
+parseType :: Text -> Type -> Doc a
+parseType value ty = case ty of
+  Variable (Name t) -> text t <> ".from_json(" <> text value <> ")"
+  GroundT gr -> case gr of
+    StringT -> "str(" <> text value <> ")"
+    BoolT -> "bool(" <> text value <> ")" -- TODO: This is wrong
+    IntT -> "int(" <> text value <> ")"
+    LongT -> "int(" <> text value <> ")"
+    DoubleT -> "float(" <> text value <> ")"
+    UUIDT -> "UUID(hex=" <> text value <> ")"
+    DateT -> "datetime.date()"
+    DateTimeT -> "datetime.datetime()"
+  ListT ty' -> "[" <> parseType "v" ty' <> " for v in " <> text value <> "]"
+  MaybeT ty' -> "(lambda v: " <> parseType "v" ty' <> " if v is not None else None)(" <> text value <> ")"
