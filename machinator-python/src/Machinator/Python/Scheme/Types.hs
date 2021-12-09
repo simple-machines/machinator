1a54551dfcf8ae1b0ad641cee53991727db67356
Swetha <sraj@simplemachines.com.au>
2021-12-09 14:46:22 +1100

    workaround for map of feature data

--- machinator-python/src/Machinator/Python/Scheme/Types.hs
+++ machinator-python/src/Machinator/Python/Scheme/Types.hs
@@ -57,6 +57,7 @@ renderModule fp mn@(ModuleName n) imports defs =
     , "import logging  # noqa: F401"
     , "import typing  # noqa: F401"
     , "import uuid  # noqa: F401"
+    , "from anaml_client.utils.object_json import JsonObject  # Workaround for Map of feature data"
     ]
     <> maybe mempty (("":) . fmap renderImport . M.toList) (mfilter (not . null) $ M.lookup mn imports)
     <> with defs Codegen.genTypesV1
